24056e61b19b5000c69424322362e39d50e1b80e
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-05-07 22:30:34 +0800

    learn about redux async requiest

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
+import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import reducer from './reducers';
+import App from './containers/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+const middleware = [thunk];
+if (process.env.NODE_ENV !== 'production') {
+	middleware.push(createLogger())
+}
+
+const store = createStore(reducer, applyMiddleware(...middleware))
+
+render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
